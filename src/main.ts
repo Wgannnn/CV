e55f104a75c79ab2d43f8ffeefe3f91dc51d4f6c
Wiktor Wagan <wiktor.wagan@gmail.com>
2025-12-17 20:16:54 +0100

    Fixed the CSS and made it more phone friendly

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
+import { provideRouter, withHashLocation } from '@angular/router';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(App);
+bootstrapApplication(App, {
+  providers: [
+    provideRouter(routes, withHashLocation())
+  ]
+});
