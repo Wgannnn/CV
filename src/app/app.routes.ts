a232697e59d30454b5808d86c4d0c8d110a9491b
Wiktor Wagan <wiktor.wagan@gmail.com>
2025-12-23 20:46:13 +0100

    Deleted Skills

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,11 @@ import { Introduction } from './introduction/introduction';
 import { ReadAboutMe } from './read-about-me/read-about-me';
 import { CareerHistory } from './career-history/career-history';
 import { Contact } from './contact/contact';
-import { Skills } from './skills/skills';
 
 export const routes: Routes = [
   { path: '', component: Introduction, data: { animation: 'Home' } },
   { path: 'about', component: ReadAboutMe, data: { animation: 'About' } },
   { path: 'career', component: CareerHistory, data: { animation: 'Career' } },
-  { path: 'skills', component: Skills, data: { animation: 'Skills' } },
   { path: 'contact', component: Contact, data: { animation: 'Contact' } },
   { path: '**', redirectTo: '' }
 ];
